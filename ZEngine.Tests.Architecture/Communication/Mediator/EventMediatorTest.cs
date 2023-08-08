2f2024c34b82c1c68dbf5e1203a4c586d9f48944
Jan Zechovský <email@zechy.cz>
2023-08-08 16:34:51 +0200

    Added test of event mediator.

---
+++ ZEngine.Tests.Architecture/Communication/Mediator/EventMediatorTest.cs
@@ -0,0 +1,37 @@
+using FluentAssertions;
+using NUnit.Framework;
+using ZEngine.Architecture.Communication.Events;
+
+namespace ZEngine.Tests.Architecture.Communication.Mediator;
+
+public class EventMediatorTest
+{
+    /// <summary>
+    /// Test basic event mediator flow.
+    /// </summary>
+    [Test]
+    public void Test_Subscribing()
+    {
+        bool wasCalled = false;
+        EventMediator mediator = new();
+        mediator.Subscribe<TestMessage>(Receiver);
+        mediator.Notify(new TestMessage());
+        wasCalled.Should().BeTrue();
+        
+        wasCalled = false;
+        mediator.Unsubscribe<TestMessage>(Receiver);
+        mediator.Notify(new TestMessage());
+        wasCalled.Should().BeFalse();
+        return;
+
+        void Receiver(TestMessage message)
+        {
+            wasCalled = true;
+        }
+    }
+    
+    private class TestMessage : IEventMessage
+    {
+        
+    }
+}
