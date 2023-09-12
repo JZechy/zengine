14e86ae69cfdeb3946a98d74b21b9b9d262475c6
Jan Zechovský <email@zechy.cz>
2023-09-12 14:50:17 +0200

    Created basic fixture and test factory.

---
+++ ZEngine.Testing/ITestFactory.cs
@@ -0,0 +1,15 @@
+using ZEngine.Core.Game;
+
+namespace ZEngine.Testing;
+
+/// <summary>
+/// Describes the factory that is used to create a new test set-up.
+/// </summary>
+public interface ITestFactory
+{
+    /// <summary>
+    /// Builds the game environment from factory.
+    /// </summary>
+    /// <returns></returns>
+    IGameManager Build();
+}
