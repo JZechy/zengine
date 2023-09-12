f650c8e8748952ddddd83630fe65362916d22fb4
Jan Zechovský <email@zechy.cz>
2023-09-12 16:52:53 +0200

    Created basic thread synchronization system.

---
+++ ZEngine.Systems.ThreadSynchronization/Operations/IAsyncOperation.cs
@@ -0,0 +1,17 @@
+﻿namespace ZEngine.Systems.ThreadSynchronization.Operations;
+
+/// <summary>
+/// Encapsules the asynchronous operation that needs to be synchronised with the game loop call.
+/// </summary>
+public interface IAsyncOperation
+{
+    /// <summary>
+    /// Invokes the operation.
+    /// </summary>
+    void Invoke();
+
+    /// <summary>
+    /// Cancels the operation.
+    /// </summary>
+    void Cancel();
+}
---
+++ ZEngine.Systems.ThreadSynchronization/ThreadSynchronizationSystem.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Concurrent;
+using ZEngine.Core;
+using ZEngine.Systems.ThreadSynchronization.Operations;
+
+namespace ZEngine.Systems.ThreadSynchronization;
+
+/// <summary>
+/// This system synchronizes calls from other threads in a safer way to ensure multi-threading stability with the game loop. 
+/// </summary>
+public class ThreadSynchronizationSystem : IGameSystem
+{
+    /// <summary>
+    /// Queue containing all registered operations.
+    /// </summary>
+    private readonly ConcurrentQueue<IAsyncOperation> _operations = new();
+    
+    /// <inheritdoc />
+    public int Priority => 99;
+
+    /// <inheritdoc />
+    public void Initialize()
+    {
+        ThreadSynchronization.CreateInstance(this);
+    }
+
+    /// <inheritdoc />
+    public void Update()
+    {
+        while (_operations.TryDequeue(out IAsyncOperation? operation))
+        {
+            operation.Invoke();
+        }
+    }
+
+    /// <inheritdoc />
+    public void CleanUp()
+    {
+        while (_operations.TryDequeue(out IAsyncOperation? operation))
+        {
+            operation.Cancel();
+        }
+    }
+    
+    /// <summary>
+    /// Registers new async operation.
+    /// </summary>
+    /// <param name="operation"></param>
+    public void RegisterOperation(IAsyncOperation operation)
+    {
+        _operations.Enqueue(operation);
+    }
+}
